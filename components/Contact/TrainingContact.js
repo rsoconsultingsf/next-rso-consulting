509d43ed60e5b34d44ff9e280ac7501ac5b8a409
alkalynesolutions <andrew@rso-consulting.com>
2023-09-13 19:49:11 -0700

    Add TrainingContact component

---
+++ components/Contact/TrainingContact.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Card from "../UI/Cards/Card";
+import SectionLabel from "../UI/Labels/SectionLabel";
+// import TrainingContactForm from "../Forms/TrainingContactForm";
+
+const TrainingContactSection = (props) => {
+  return (
+    <section id="contact">
+      <div className="container center">
+        {!props.noLabel && (
+          <SectionLabel orange>{props.title || "Get In Touch"}</SectionLabel>
+        )}
+        {props.contactHeader ? props.contactHeader : <p></p>}
+      </div>
+      <div className="container">
+        <div className="desktop">
+          <Card>{/* <TrainingContactForm /> */}</Card>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ width: "100%" }}>
+            <div
+              className="mobile"
+              style={{ paddingBottom: "30px" }}
+            >
+              <Card>{/* <TrainingContactForm /> */}</Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TrainingContactSection;
