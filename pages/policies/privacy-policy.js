81eb112cf449178f060b39346bbc219706903368
alkalynesolutions <andrew@alkalyne.com>
2024-10-31 12:42:05 -0700

    create policies directory

--- pages/policies/privacy-policy.js
+++ pages/policies/privacy-policy.js
@@ -91,12 +91,6 @@ const PrivacyPolicy = () => {
             <b>Cookies</b>
           </h2>
         </div>
-        {/* <Script
-          id="CookieDeclaration"
-          src="https://consent.cookiebot.com/d447ec29-dc7c-46bc-90e6-68f2cb28826c/cd.js"
-          type="text/javascript"
-          async
-          data-georegions="{'region':'US-06','cbid':'4e3d269b-0766-46f9-beec-61d4a75119a5'},{'region':'US-51','cbid':'a6c78df2-c7a8-447b-9162-39f20c77c5d3'},{'region':'AT, BE, BG, CY, CZ, DE, DK, EE, ES, FI, FR, GB, GR, HR, HU, IE, IT, LT, LU, LV, MT, NL, PL, PT, RO, SE, SI, SK','cbid':'a6546862-c312-4440-8228-972aa88aa07f'},{'region':'BR','cbid':'86ef25b5-138b-46c5-807d-05666a7d21ff'}"></Script> */}
       </section>
       <section>
         <div className="container">
