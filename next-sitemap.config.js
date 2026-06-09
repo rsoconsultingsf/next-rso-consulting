22b182fee54d927ed8d2d1fe4525eaf4cad6b2a8
alkalynesolutions <andrew@alkalyne.com>
2026-06-09 09:32:11 -0700

    Fix sitemap URL: hardcode production domain as default

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://example.com",
+  siteUrl: process.env.SITE_URL || "https://www.rso-consulting.com",
   generateIndexSitemap: false,
   generateRobotsTxt: true, // (optional)
   // ...other options
